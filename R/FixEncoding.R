1ca794544020b337663df2e2e81c6e544071d93f
Justin <justin@ratchet.nceas.ucsb.edu>
2016-08-17 10:16:45 -0700

    Update function names in main package script

--- R/FixEncoding.R
+++ R/FixEncoding.R
@@ -1,6 +1,6 @@
 #' FixEncoding: A package for locating and replacing invalid UTF-8 bytes
 #'
-#' The FixEncoding package provides three functions: enc_check and enc_check2
+#' The FixEncoding package provides three functions: check_column_encoding.R and enc_check2
 #' for locating non-UTF-8 bytes, and ascii_replace for replacing invalid UTF-8
 #' bytes with valid bytes.
 #'
